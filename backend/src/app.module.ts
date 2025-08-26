15911e606953e270d9f4a3f8d2f4e565d232a056
ahmed medhat <am510836@gmail.com>
2025-08-26 20:19:34 +0300

    add: MongoDB config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
   imports: [ ConfigModule.forRoot({
-    envFilePath: `.env.${process.env.NODE_ENV}`,
+    envFilePath: '.env',
     isGlobal:true 
-  })],
+  }),
+  MongooseModule.forRootAsync({
+    inject:[ConfigService],
+    useFactory: (configService :ConfigService) => ({
+      uri: configService.get<string>("MONGO_URI")
+    })
+  })
+],
   controllers: [],
   providers: [],
 })
