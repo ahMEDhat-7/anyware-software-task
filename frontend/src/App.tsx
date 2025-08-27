e69992c3d18b744a9ad973e45027d7ac980167e1
ahmed medhat <am510836@gmail.com>
2025-08-27 10:51:26 +0300

    ad: home & dashboard pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
 
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/dashboard' element={<Dashboard />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
